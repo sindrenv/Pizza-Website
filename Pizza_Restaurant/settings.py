6f16be7edd71173501dbce5a2244a6b91e11f6fc
sindrenv <sindrevangen02@gmail.com>
2025-03-20 14:43:01 +0100

    Changes today

--- Pizza_Restaurant/settings.py
+++ Pizza_Restaurant/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +123,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# settings.py
+
+import os
+
+# Media settings
+MEDIA_URL = '/media/'  # URL prefix for media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Folder where media files will be stored
--- Pizza_Restaurant/urls.py
+++ Pizza_Restaurant/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from Pizza_App import views  
@@ -25,3 +26,8 @@ urlpatterns = [
     path('menu/', views.menu, name='menu'),
     path('order/', views.order, name='place_order'),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
